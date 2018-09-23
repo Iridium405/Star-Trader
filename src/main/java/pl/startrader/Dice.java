60082faba6b49cbfba4493fb6aceabce9a175b37
Łukasz Rau <luk.rau.01@gmail.com>
2018-09-23 12:07:52 +0200

    Dice class updated on different kind of throws.

--- src/main/java/pl/startrader/Dice.java
+++ src/main/java/pl/startrader/Dice.java
@@ -1,5 +1,7 @@
 package pl.startrader;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Dice {
@@ -16,8 +18,40 @@ public class Dice {
         return instance;
     }
 
-    public int throwDice (int numberOfSides){
+
+    public Integer throwDice (Integer numberOfSides){
         Random dice = new Random();
-        return dice.nextInt(numberOfSides) +1;
+        try {
+            return dice.nextInt(numberOfSides) +1;
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Number of sides must be above 0.");
+            return null;
+        }
+    }
+
+
+    public Integer throwModifiedDice (Integer numberOfSides, Integer modification, boolean bordered) {
+        Integer diceRoll = throwDice(numberOfSides);
+        System.out.println(diceRoll);
+
+        if(!bordered) {
+            return diceRoll + modification;
+        } else {
+            if((diceRoll+modification) > 0) {
+                return diceRoll + modification;
+            } else {
+                return 1;
+            }
+        }
+    }
+
+
+    public List<Integer> throwMultipleDices (int rolls, int numberOfSides ) {
+        List<Integer> rollsList = new ArrayList<Integer>();
+        for (int roll = 1; roll <= rolls; roll++) {
+            Integer diceRoll = throwDice(numberOfSides);
+            rollsList.add(diceRoll);
+        }
+        return rollsList;
     }
 }
