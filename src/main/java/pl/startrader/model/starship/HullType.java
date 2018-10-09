82bdec73355d03db40961a1eb3a5a47de284933f
Łukasz Rau <luk.rau.01@gmail.com>
2018-10-09 16:13:58 +0200

    Durability added.

--- src/main/java/pl/startrader/model/starship/HullType.java
+++ src/main/java/pl/startrader/model/starship/HullType.java
@@ -5,30 +5,39 @@ import static pl.startrader.model.starship.HullSize.*;
 
 public enum HullType {
 
-    PICCOLO     ("Piccolo", E, 1, 1, XS, 5),
-    CLARINET    ("Clarinet", E, 1, 1, S, 10),
-    VIOLIN      ("Violin", D, 1, 2, S, 14),
-    HARP        ("Harp", D, 1, 3, S, 18),
-    STINGRAY    ("Stingray", C, 2, 5, M, 20);
+    PICCOLO     ("Piccolo",     E, 1, 1,    XS, 5,  15),
+    CLARINET    ("Clarinet",    E, 1, 1,    S,  10, 20),
+    VIOLIN      ("Violin",      D, 1, 2,    S,  14, 20),
+    HARP        ("Harp",        D, 1, 3,    S,  18, 20),
+    STINGRAY    ("Stingray",    C, 2, 5,    M,  20, 35),
+    HARMONY     ("Harmony",     C, 2, 4,    M,  24, 25),
+    CLAW        ("Claw",        B, 3, 7,    L,  32, 40);
 
 
 
     private String name;
+
     private CrewClass minimumCrewClass;
     private Integer minCrewMembers;
     private Integer maxCrewMembers;
+
     private HullSize hullSize;
     private Integer moduleCapacity;
+    private Integer durability;
+
 
 
     HullType(String name, CrewClass minimumCrewClass, Integer minCrewMembers, Integer maxCrewMembers,
-             HullSize hullSize, Integer moduleCapacity) {
+             HullSize hullSize, Integer moduleCapacity, Integer durability) {
         this.name = name;
+
         this.minimumCrewClass = minimumCrewClass;
         this.minCrewMembers = minCrewMembers;
         this.maxCrewMembers = maxCrewMembers;
+
         this.hullSize = hullSize;
         this.moduleCapacity = moduleCapacity;
+        this.durability = durability;
     }
 
 
@@ -55,5 +64,9 @@ public enum HullType {
 
     public Integer getModuleCapacity() {
         return moduleCapacity;
+    }
+
+    public Integer getDurability() {
+        return durability;
     }
 }
