f07e6ce21c029afc250cd3badfedd2544b36107e
Łukasz Rau <luk.rau.01@gmail.com>
2018-10-09 16:40:33 +0200

    Character statistic fields and upgrading methods added.

--- src/main/java/pl/startrader/model/character/Crew.java
+++ src/main/java/pl/startrader/model/character/Crew.java
@@ -10,8 +10,14 @@ public class Crew extends Character {
     private Integer mediumWeaponDamage;
     private Integer heavyWeaponDamage;
     private Integer hullDurability;
+    private Integer mechanics;
+    private Integer hitPoints;
+    private Integer attack;
+    private Integer defence;
     private Integer experience;
     private Integer level;
+    private Integer skillPoints;
+
 
 
     public Crew(String name, Integer criminalConnections, Integer politicalConnections, Integer economicConnections) {
@@ -21,7 +27,132 @@ public class Crew extends Character {
         this.mediumWeaponDamage = 0;
         this.heavyWeaponDamage = 0;
         this.hullDurability = 0;
+        this.mechanics = 0;
+        this.hitPoints = 1;
+        this.attack = 1;
+        this.defence = 3;
         this.experience = 0;
         this.level = 1;
+        this.skillPoints = 1;
+    }
+
+
+
+    public CrewClass getCrewClass() {
+        return crewClass;
+    }
+
+
+
+    public Integer getLightWeaponDamage() {
+        return lightWeaponDamage;
+    }
+
+    public void addSkillPoint_LightWeaponDamage() {
+        if (this.skillPoints >= 1) {
+            this.lightWeaponDamage += 1;
+            this.skillPoints -= 1;
+        }
+    }
+
+
+
+    public Integer getMediumWeaponDamage() {
+        return mediumWeaponDamage;
+    }
+
+    public void addSkillPoint_MediumWeaponDamage() {
+        if (this.skillPoints >= 1) {
+            this.mediumWeaponDamage += 1;
+            this.skillPoints -= 1;
+        }
+    }
+
+
+
+    public Integer getHeavyWeaponDamage() {
+        return heavyWeaponDamage;
+    }
+
+    public void addSkillPoint_HeavyWeaponDamage() {
+        if (this.skillPoints >= 1) {
+            this.heavyWeaponDamage += 1;
+            this.skillPoints -= 1;
+        }
+    }
+
+
+
+    public Integer getHullDurability() {
+        return hullDurability;
+    }
+
+    public void addSkillPoint_HullDurabiity() {
+        if (this.skillPoints >= 1) {
+            this.hullDurability += 1;
+            this.skillPoints -= 1;
+        }
+    }
+
+
+
+    public Integer getMechanics() {
+        return mechanics;
+    }
+
+    public void addSkillPoint_Mechanics() {
+        if (this.skillPoints >= 1) {
+            this.mechanics += 1;
+            this.skillPoints -= 1;
+        }
+    }
+
+
+
+    public Integer getHitPoints() {
+        return hitPoints;
+    }
+
+    public void addSkillPoint_HitPoints() {
+        if (this.skillPoints >= 1) {
+            this.hitPoints += 1;
+            this.skillPoints -= 1;
+        }
+    }
+
+
+
+    public Integer getAttack() {
+        return attack;
+    }
+
+    public void addSkillPoint_Attack() {
+        if (this.skillPoints >= 1) {
+            this.attack += 1;
+            this.skillPoints -= 1;
+        }
+    }
+
+
+
+    public Integer getDefence() {
+        return defence;
+    }
+
+    public void addSkillPoint_Defence() {
+        if (this.skillPoints >= 1) {
+            this.defence += 1;
+            this.skillPoints -= 1;
+        }
+    }
+
+
+
+    public Integer getExperience() {
+        return experience;
+    }
+
+    public Integer getLevel() {
+        return level;
     }
 }
