64146f0b9a9e6f027c4e2377d75d2204e0284907
Iridium <luk.rau.01@gmail.com>
2018-10-17 18:58:39 +0200

    Travel cost factor added.

--- src/main/java/pl/startrader/model/starship/HullSize.java
+++ src/main/java/pl/startrader/model/starship/HullSize.java
@@ -2,10 +2,20 @@ package pl.startrader.model.starship;
 
 public enum HullSize {
 
-    XXL,
-    XL,
-    L,
-    M,
-    S,
-    XS;
+    XXL (15.0),
+    XL  (10.0),
+    L   (6.0),
+    M   (4.0),
+    S   (2.0),
+    XS  (1.0);
+
+    private Double travelCostFactor;
+
+    HullSize(Double travelCostFactor) {
+        this.travelCostFactor = travelCostFactor;
+    }
+
+    public Double getTravelCostFactor() {
+        return travelCostFactor;
+    }
 }
